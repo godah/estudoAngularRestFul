4caa01bd0b61c128db1235fe3cdadd3c69379058
'luciano' <'luciano@ematex.com.br'>
2017-09-22 15:22:17 -0300

    update

---
+++ WebContent/resources/script/filterEmTabela.js
@@ -0,0 +1,30 @@
+var app = angular.module('lojax', ['ngRoute']);
+
+app.controller("filterController2", function($scope) {
+	$scope.friends = [
+		{
+		name: "Mario",
+		lastname: "Souza",
+		age: 20,
+		},
+		
+		{
+		name: "Maria",
+		lastname: "Quermina",
+		age: 89,
+		},
+
+		{
+		name: "Paulo",
+		lastname: "Matos",
+		age: 52,
+		},
+		
+		{
+			name: "Joaquim",
+			lastname: "Luzia",
+			age: 33,
+		},
+	];
+	
+});
